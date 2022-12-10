78f0ec032fec0a0a815a50950ec8e90fe8563098
afn <hey@afn.lol>
2022-12-10 15:50:23 -0500

    feat(instagram): dark mode redirect

---
+++ instagram/dark-mode-redirect.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Instagram Dark Mode Redirect
+// @description Redirect to Instagram's official dark theme site.
+// @version     1.0
+// @author      afn (https://github.com/xafn)
+// @namespace   https://github.com/xafn/userscripts/tree/main/instagram/dark-mode-redirect
+// @license     MIT
+// @match       https://www.instagram.com/
+// @include     *://www.instagram.com/*
+// @run-at      document-body
+// @grant       none
+// ==/UserScript==
+
+let css = `
+  #splash-screen {
+    background-color: var(--ig-secondary-background) !important;
+  }
+`;
+
+let styleSheet = document.createElement("style");
+styleSheet.innerText = css;
+document.head.appendChild(styleSheet);
+
+if (window.location.search !== "?theme=dark") {
+  window.location.search = "?theme=dark";
+}
